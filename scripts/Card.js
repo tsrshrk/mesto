f189dcd5f0bd65a4f5db706989443233fd3924d7
Aleksey Churilov <saulvz@yandex.ru>
2022-08-11 13:16:28 +0300

    feat: добавить класс Card

---
+++ scripts/Card.js
@@ -0,0 +1,55 @@
+import { handleShowImg } from './popups.js';
+
+export default class Card {
+  constructor(text, image, templateSelector) {
+    this._text = text;
+    this._image = image;
+    this._templateSelector = templateSelector;
+  }
+
+  _getTemplate() {
+    const cardElement = document.querySelector(this._templateSelector)
+    .content.querySelector('.gallery__card').cloneNode(true);
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._elementTitle = this._element.querySelector('.gallery__title');
+    this._elementImage = this._element.querySelector('.gallery__image');
+    this._elementLikeButton = this._element.querySelector('.gallery__button-like');
+    this._elementDeleteButton = this._element.querySelector('.gallery__button-delete');
+
+    this._elementImage.src = this._image;
+    this._elementImage.alt = this._text;
+    this._elementTitle.textContent = this._text;
+
+    this._setEventListeners();
+
+    return this._element;
+  }
+
+  _handleLikeClick() {
+    this._elementLikeButton.classList.toggle('gallery__button-like_active');
+  }
+
+  _handleDeleteClick() {
+    this._element.remove();
+  }
+
+  _handleShowPopupClick() {
+    handleShowImg(this._image, this._text);
+  }
+
+  _setEventListeners() {
+    this._elementImage.addEventListener('click', () => {
+      this._handleShowPopupClick();
+    });
+    this._elementLikeButton.addEventListener('click', () => {
+      this._handleLikeClick();
+    });
+    this._elementDeleteButton.addEventListener('click', () => {
+      this._handleDeleteClick();
+    });
+  }
+}
--- scripts/popups.js
+++ scripts/popups.js
@@ -3,42 +3,42 @@ const truescaleImg = document.querySelector('.popup__truscale-img');
 const truescaleImgTitle = document.querySelector('.popup__truscale-img-title');
 
 const fillImgPopup = (link, name) => {
-    truescaleImg.src = link;
-    truescaleImg.alt = name;
-    truescaleImgTitle.textContent = name;
+  truescaleImg.src = link;
+  truescaleImg.alt = name;
+  truescaleImgTitle.textContent = name;
 }
 
 export function openPopup(popup) {
-    popup.classList.add('popup_opened');
-    window.addEventListener('keydown', handleEscape);
+  popup.classList.add('popup_opened');
+  window.addEventListener('keydown', handleEscape);
 }
 
 export function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-    window.removeEventListener('keydown', handleEscape);
+  popup.classList.remove('popup_opened');
+  window.removeEventListener('keydown', handleEscape);
 }
 
 const handleEscape = (evt) => {
-    if (evt.key === 'Escape') {
-        const popupOpened = document.querySelector('.popup_opened');
-        closePopup(popupOpened);
-    }
+  if (evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }
 }
 
 export function handleShowImg(link, name) {
-    fillImgPopup(link, name);
-    openPopup(imgPopup);
+  fillImgPopup(link, name);
+  openPopup(imgPopup);
 }
 
 const popupList = Array.from(document.querySelectorAll('.popup'));
 
 popupList.forEach((listElement) => {
-    listElement.addEventListener('mousedown', (evt) => {
-        if (evt.target.classList.contains('popup_opened')) {
-            closePopup(listElement);
-        }
-        if (evt.target.classList.contains('popup__button-close')) {
-            closePopup(listElement);
-        }
-    })
+  listElement.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(listElement);
+    }
+    if (evt.target.classList.contains('popup__button-close')) {
+      closePopup(listElement);
+    }
+  })
 })
